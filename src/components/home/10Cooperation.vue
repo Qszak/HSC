2750d0429ea01e93849f89a6395b69095c903c11
Qszak <jedrzej.kuszak@gmail.com>
2024-10-09 12:50:53 +0200

    09/10/2024

--- src/components/home/10Cooperation.vue
+++ src/components/home/10Cooperation.vue
@@ -1,39 +1,125 @@
 <template>
     <section class="cooperations">
         <div class="claim">Współprace</div>
-        <div class="brands">
-            <div class="logo">
-                <img src="\images\Borkowski logo rgb.png" alt="" />
-            </div>
-            <div class="logo">
-                <img
-                    src="\images\CUDO_Logo_Wersja_Podstawowa_White_Bg.png"
-                    alt=""
-                />
-            </div>
-            <div class="logo">
-                <img src="\images\IMG_20210822_235810_110.jpg" alt="" />
-            </div>
-            <div class="logo">
-                <img src="\images\imprimature_logo_czarne_bez_tla.png" alt="" />
-            </div>
-            <div class="logo">
-                <img src="\images\ocludo logo czarne.png" alt="" />
-            </div>
-            <div class="logo">
-                <img src="\images\sandras-kolorBIGPNG.png" alt="" />
-            </div>
-        </div>
+        <swiper
+            :loop="true"
+            :autoplay="true"
+            :slidesPerView="3"
+            :spaceBetween="20"
+            modules="[Autoplay]"
+            class="brands"
+        >
+            <swiper-slide class="logo">
+                <a href="https://www.borkowskiwspolnicy.pl/">
+                    <img
+                        src="/images/Borkowski logo rgb.png"
+                        alt="Logo Borkowski i Wspólnicy"
+                    />
+                </a>
+            </swiper-slide>
+
+            <swiper-slide class="logo">
+                <a href="https://cudostudio.pl/">
+                    <img src="/images/cudo.png" alt="Logo Studio CUDO" />
+                </a>
+            </swiper-slide>
+
+            <swiper-slide class="logo">
+                <a href="https://iron-gym.pl/">
+                    <img src="/images/irongym.jpg" alt="Logo IronGym" />
+                </a>
+            </swiper-slide>
+
+            <swiper-slide class="logo">
+                <a href="https://berserkersteamwroclaw.com/">
+                    <img
+                        src="/images/berserkers.jpg"
+                        alt="Logo Berserkers Team"
+                    />
+                </a>
+            </swiper-slide>
+
+            <swiper-slide class="logo">
+                <a href="https://sandras.fit/">
+                    <img src="/images/sandras.png" alt="Logo Sandra's" />
+                </a>
+            </swiper-slide>
+
+            <swiper-slide class="logo">
+                <a href="https://imprimature.co/">
+                    <img src="/images/imprimature.png" alt="Logo Imprimature" />
+                </a>
+            </swiper-slide>
+
+            <swiper-slide class="logo">
+                <a href="https://ocludo.pl/">
+                    <img src="/images/ocludo.png" alt="Logo Ocludo" />
+                </a>
+            </swiper-slide>
+        </swiper>
     </section>
 </template>
 
 <script>
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/swiper-bundle.css";
+import { Autoplay } from "swiper";
+
 export default {
     name: "Cooperation",
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
+    modules: [Autoplay],
 };
 </script>
 
 <style scoped>
+.cooperations {
+    padding: 50px 20px;
+    background-color: #f9f9f9;
+    text-align: center;
+}
+
+.cooperations .claim {
+    font-size: 2rem;
+    font-weight: bold;
+    margin-bottom: 30px;
+    color: #333;
+}
+
+.swiper {
+    width: 100%;
+    height: auto;
+}
+
+.swiper-slide {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.logo {
+    width: 180px; /* Dostosuj szerokość logo */
+    height: 500px;
+    padding: 10px;
+    transition: transform 0.3s ease;
+}
+
+.logo img {
+    max-width: 100%;
+    height: auto;
+    object-fit: contain;
+    filter: grayscale(100%);
+    transition: filter 0.3s ease, transform 0.3s ease;
+}
+
+.logo:hover img {
+    filter: grayscale(0);
+    transform: scale(1.1); /* Powiększenie przy hover */
+}
+
 @media (max-width: 640px) {
     section.cooperations {
         display: flex;
@@ -76,6 +162,19 @@ export default {
     }
 }
 
+@media (max-width: 960px) {
+    .swiper {
+        padding: 20px;
+    }
+
+    .swiper-slide {
+        width: 120px;
+    }
+
+    .logo {
+        width: 120px; /* Mniejsza szerokość na mobilnych */
+    }
+}
 @media (min-width: 640px) and (max-width: 960px) {
     section.cooperations {
         display: flex;
@@ -149,8 +248,8 @@ export default {
         align-self: stretch;
     }
     .logo {
-        width: 5rem;
-        height: 5rem;
+        width: 10rem;
+        height: 10rem;
     }
     img {
         width: 100%;
